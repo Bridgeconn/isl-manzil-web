9383388f72d586dc07b9b20f448070c0a9065d7b
Ketan Kumar Baboo <ketan.baboo@bridgeconn.com>
2025-05-14 13:35:56 +0530

    Implemented fetch bible content based on dropdown selection and some changes regarding layout style properties

--- src/types/bible.ts
+++ src/types/bible.ts
@@ -1,5 +1,17 @@
+import { BookOption, ChapterOption, VerseOption } from './Navigation';
 export interface VersificationData {
   maxVerses: {
     [bookCode: string]: string[];
   };
+}
+
+export interface BibleVerseDisplayProps {
+  selectedBook: BookOption | null;
+  selectedChapter: ChapterOption | null;
+  selectedVerse?: VerseOption | null;
+}
+
+export interface VerseData {
+  verse: string;
+  text: string;
 }
