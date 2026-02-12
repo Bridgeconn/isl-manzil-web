a148cd7ebaea2150a9b3f4a850f27d947de7abdb
Adity Singh <adity.singh@bridgeconn.com>
2026-02-12 16:21:07 +0530

    added ui chnages in video player in landing page

--- web-app/src/pages/Landing.tsx
+++ web-app/src/pages/Landing.tsx
@@ -2,30 +2,37 @@ import BibleImg from "@/assets/bible.png";
 import DictionaryImg from "@/assets/dictionary.png";
 import ProjectImg from "@/assets/project.png";
 import { useNavigate } from "react-router-dom";
+import CleanVimeoPlayer from "@/components/CleanVimeoPlayer";
+
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
-    <div className="bg-white flex justify-end pr-4 pt-4 pb-4 ">
+    <div className="bg-white flex gap-6 pr-4 pt-4 pb-4">
 
       {/* Pale blue frame */}
-      <div className="bg-blue-200/20 rounded-[28px] p-4">
+      <div className="flex-[1.4] mt-18 flex justify-center">
+        <div className="aspect-video w-full">
+          <CleanVimeoPlayer videoId={1105756880} />
+        </div>
+      </div>
+      <div className="bg-gray-500/30 rounded-[28px] p-4">
 
-        <div className="flex flex-col gap-2 w-[420px]">
+        <div className="flex flex-col gap-2 w-[360px] mx-auto">
 
           {/* Tile 1 */}
           <div
             onClick={() => navigate("/HomePage")}
             className="text-center cursor-pointer"
           >
-            <div className="w-full aspect-[16/9] overflow-hidden rounded-2xl">
+            <div className="w-full aspect-[16/10] overflow-hidden rounded-4xl">
               <img
                 src={BibleImg}
                 alt="Bible"
                 className="w-full h-full object-cover"
               />
             </div>
-            <p className="mt-2 text-blue-900 font-semibold text-xl">
+            <p className="mt-2 text-black font-semibold text-xl">
               Bible
             </p>
           </div>
@@ -35,15 +42,15 @@ const Landing = () => {
             onClick={() => navigate("/dictionary")}
             className="text-center cursor-pointer"
           >
-            <div className="w-full aspect-[16/9] overflow-hidden rounded-2xl">
+            <div className="w-full aspect-[16/10] overflow-hidden rounded-4xl">
 
               <img
                 src={DictionaryImg}
                 alt="Dictionary"
-                className="w-full rounded-xl"
+                className="w-full h-full object-cover "
               />
             </div>
-            <p className="mt-2 text-blue-900 font-semibold text-xl">
+            <p className="mt-2 text-black font-semibold text-xl">
               Dictionary
             </p>
           </div>
@@ -53,15 +60,15 @@ const Landing = () => {
             onClick={() => navigate("/projects")}
             className="text-center cursor-pointer"
           >
-            <div className="w-full aspect-[16/9] overflow-hidden rounded-2xl">
+            <div className="w-full aspect-[16/10] overflow-hidden rounded-4xl">
 
               <img
                 src={ProjectImg}
                 alt="The Bible Projects"
-                className="w-full rounded-xl"
+                className="w-full h-full object-cover"
               />
             </div>
-            <p className="mt-2 text-blue-900 font-semibold text-xl">
+            <p className="mt-2 text-black font-semibold text-xl">
               The Bible Projects
             </p>
           </div>
