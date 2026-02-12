19803fc64f4d753cf0082df051cc709a20b1a3dc
Adity Singh <adity.singh@bridgeconn.com>
2026-02-12 11:04:47 +0530

    added about us video in landing page

---
+++ web-app/src/pages/Landing.tsx
@@ -0,0 +1,73 @@
+import BibleImg from "@/assets/bible.png";
+import DictionaryImg from "@/assets/dictionary.png";
+import ProjectImg from "@/assets/project.png";
+import { useNavigate } from "react-router-dom";
+import CleanVimeoPlayer from "@/components/CleanVimeoPlayer";
+
+const Landing = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="bg-white flex justify-end pr-4 pt-4 pb-4 ">
+
+      <div className="flex-1 mt-16 flex justify-center">
+        <div className="aspect-video w-[75%]">
+          <CleanVimeoPlayer videoId={1105756880} />
+        </div>
+      </div>
+      <div className="bg-blue-200/20 rounded-[28px] p-4">
+        <div className="flex flex-col gap-2 w-[420px]">
+
+          <div
+            onClick={() => navigate("/HomePage")}
+            className="text-center cursor-pointer"
+          >
+            <div className="w-full aspect-[16/9] overflow-hidden rounded-2xl">
+              <img
+                src={BibleImg}
+                alt="Bible"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="mt-2 text-blue-900 font-semibold text-xl">
+              Bible
+            </p>
+          </div>
+          <div
+            onClick={() => navigate("/dictionary")}
+            className="text-center cursor-pointer"
+          >
+            <div className="w-full aspect-[16/9] overflow-hidden rounded-2xl">
+              <img
+                src={DictionaryImg}
+                alt="Dictionary"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="mt-2 text-blue-900 font-semibold text-xl">
+              Dictionary
+            </p>
+          </div>
+
+          <div
+            onClick={() => navigate("/projects")}
+            className="text-center cursor-pointer"
+          >
+            <div className="w-full aspect-[16/9] overflow-hidden rounded-2xl">
+              <img
+                src={ProjectImg}
+                alt="The Bible Projects"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <p className="mt-2 text-blue-900 font-semibold text-xl">
+              The Bible Projects
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
