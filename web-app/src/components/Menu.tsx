580510d7d4d187a69fbaa9ba4baa7f4f98d5e2f8
Adity Singh <adity.singh@bridgeconn.com>
2026-02-16 12:20:55 +0530

    adding top component in all pages

--- web-app/src/components/Menu.tsx
+++ web-app/src/components/Menu.tsx
@@ -1,30 +1,42 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate , useLocation} from "react-router-dom";
 
 export default function Menu({ onClose }: { onClose: () => void }) {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const go = (path: string) => {
     navigate(path);
     onClose();
   };
+   const showBible = location.pathname !== "/HomePage";
+   const showDictionary = location.pathname !== "/dictionary";
+   const showProjects = location.pathname !== "/projects";
+ 
   return (
     <div className="absolute w-40 left-0 bg-white border border-gray-200    ">
+      {showBible && (
       <div className="px-2 py-2 cursor-pointer hover:bg-gray-200 transition">
         <p onClick={() => go("/HomePage")} className="cursor-pointer ">
           Bible
         </p>
       </div>
+      )}
+      {showDictionary && (
       <p
         onClick={() => go("/dictionary")}
         className="px-2 py-2 cursor-pointer hover:bg-gray-200 transition "
       >
         Dictionary
       </p>
+      )}
+      {showProjects && (
       <p
         onClick={() => go("/projects")}
         className="px-2 py-2 cursor-pointer hover:bg-gray-200 transition "
       >
         The Bible Project
       </p>
+      )}
     </div>
   );
 }
